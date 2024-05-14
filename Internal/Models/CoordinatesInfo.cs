8c981e144a9882c7951b87758f7994f6348a3ba6
Tais993 <tijs@familiebeek.eu>
2024-05-14 13:41:43 +0200

    Updated models, AssetQuery, Sensor and CoordinatesInfo

---
+++ Internal/Models/CoordinatesInfo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace OpenRemoteAPI.Internal.Models;
+
+public class CoordinatesInfo
+{
+
+    [JsonProperty("coordinates")]
+    public float[] Coordinates;
+
+    [JsonProperty("type")]
+    public string Type;
+
+    public float Longitude()
+    {
+        return Coordinates[0];
+    }
+
+    public float Latitude()
+    {
+        return Coordinates[1];
+    }
+}
