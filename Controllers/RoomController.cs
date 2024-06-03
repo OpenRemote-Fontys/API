dfe8da7bb13b72976ad9c22c7378fbe94988d3e3
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-06-03 09:48:53 +0200

    Free room implementataion

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -7,15 +7,20 @@ using System.Net;
 using System;
 using Newtonsoft.Json;
 using System.Text;
+using OpenRemoteAPI.Internal.Models;
+using System.Reflection.Metadata.Ecma335;
+using Newtonsoft.Json.Linq;
+
 
 namespace OpenRemoteAPI.Controllers
 {    
     [ApiController]
     public class RoomController : Controller
     {
+
         [HttpGet]
         [Route("/Room/Free")]
-        public async Task<IActionResult> GetFreeRooms(int index)
+        public async Task<IActionResult> GetFreeRooms()
         {
             var openRemote = new OpenRemoteApi();
             var roomQuery = new AssetQueryBuilder()
@@ -24,63 +29,42 @@ namespace OpenRemoteAPI.Controllers
                 .AddTypes("RoomAsset")
                 .Build();
 
-            var rooms = await openRemote.QueryAssets(roomQuery);
+            List<Asset> rooms = await openRemote.QueryAssets(roomQuery);
 
-            var sensorQuery = new AssetQueryBuilder()
-                .SetRealmName("master")
-                .SetLimit(0)
-                .SetFilterAttribute("Room", rooms[index].Id)
-                .Build();
+            List<Asset> emptyRoomAssets = new List<Asset>();
 
-            var sensors = await openRemote.QueryAssets(sensorQuery);
-            return Ok(sensors);
-        }
+            foreach (Asset room in rooms)
+            {
+                var sensorQuery = new AssetQueryBuilder()
+                    .SetRealmName("master")
+                    .SetLimit(0)
+                    .SetFilterAttribute("Room", room.Id)
+                    .Build();
 
-        [HttpGet]
-        [Route("/Room/All")]
-        public async Task<IActionResult> GetAllRooms()
-        {
-            var openRemote = new OpenRemoteApi();
+                Asset? sensor = (await openRemote.QueryAssets(sensorQuery)).FirstOrDefault();
 
-            var assetQuery = new AssetQueryBuilder()
-                .SetRealmName("master")
-                .SetLimit(0)
-                .AddTypes("RoomAsset")
-                .Build();
+                if (sensor == null) continue;
 
-            var response = await openRemote.QueryAssets(assetQuery);
-            return Ok(response);
-        }
+                Dictionary<string, object> jsonObject = new() { { "fromTime", (DateTime.Now - TimeSpan.FromMinutes(1)).ToString("yyyy-MM-ddTHH:mm:ss.fffff") }, { "toTime", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffff") }, { "type", "string" } };
+                string json = JsonConvert.SerializeObject(jsonObject);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
-        [HttpGet]
-        [Route("/Room/Data")]
-        public async Task<IActionResult> GetRoomData(int index)
-        {
-            var openRemote = new OpenRemoteApi();
-            var roomQuery = new AssetQueryBuilder()
-                .SetRealmName("master")
-                .SetLimit(0)
-                .AddTypes("RoomAsset")
-                .Build();
+                var response = await openRemote.MakeHttpCall("asset/datapoint/" + sensor.Id + "/JSONReadings2", Internal.Requests.HttpMethod.POST, content);
+                var jsonString = await response.Content.ReadAsStringAsync();
+                
+                List<DataPoint> datapoints = JsonConvert.DeserializeObject<List<DataPoint>>(jsonString);
 
-            var rooms = await openRemote.QueryAssets(roomQuery);
+                if (IsFree(datapoints)) emptyRoomAssets.Add(room);
+            }
 
-            var sensorQuery = new AssetQueryBuilder()
-                .SetRealmName("master")
-            .SetLimit(0)
-                .SetFilterAttribute("Room", rooms[index].Id)
-                .Build();
+            List<Room> emptyRooms = new List<Room>();
+            foreach (Asset room in emptyRoomAssets)
+            {
+                var roomBounds = ((JArray)room.Attributes["roomBounds"].Value).ToObject<List<float[]>>();
+                emptyRooms.Add(new Room() { Id = room.Id, Name = room.Name, RoomBounds = roomBounds, Color = room.Attributes["color"].Value });
+            }
 
-            var sensors = await openRemote.QueryAssets(sensorQuery);
-
-            Dictionary<string, object> jsonObject = new() { { "fromTime", (DateTime.Now - TimeSpan.FromMinutes(1)).ToString("yyyy-MM-ddTHH:mm:ss.fffff") }, { "toTime", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffff") }, { "type", "string" } };
-            string json = JsonConvert.SerializeObject(jsonObject);
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var response = await openRemote.MakeHttpCall("asset/datapoint/" + sensors[0].Id + "/JSONReadings2", Internal.Requests.HttpMethod.POST, content);
-
-
-            return Ok(await response.Content.ReadAsStringAsync());
+            return Ok(emptyRooms);
         }
 
         [HttpGet]
@@ -94,5 +78,30 @@ namespace OpenRemoteAPI.Controllers
                 new Room { Id = "3", Name = "2.125", Color = "ff0000" }
             });
         }
+
+
+        /* Helper classes */
+        public class DataPoint()
+        {
+            [JsonProperty("y")] public Dictionary<string, int> frequencies { get; private set; }
+        }
+
+        /* Helper Methods */
+        private bool IsFree(List<DataPoint> datapoints) 
+        {
+            int thresholdMetCount = 0;
+            foreach (DataPoint datapoint in datapoints)
+            {
+                int totalValues = 0;
+                for (int i = datapoint.frequencies.Count - 4; i < datapoint.frequencies.Count; i++)
+                {
+                    totalValues += datapoint.frequencies.ElementAt(i).Value;
+                }
+                if (totalValues > /*5000*/50000) thresholdMetCount++;
+            }
+            return thresholdMetCount < (datapoints.Count / 2);
+        }
     }
 }
+
+// 5000Hz > 6000
